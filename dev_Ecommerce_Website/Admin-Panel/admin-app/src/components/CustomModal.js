289d55fbe77ea1407100f836e9f892d51e3e6d12
ANURAG P <anuragp212@gmail.com>
2024-04-10 11:30:50 +0530

    Update CustomModal.js

--- dev_Ecommerce_Website/Admin-Panel/admin-app/src/components/CustomModal.js
+++ dev_Ecommerce_Website/Admin-Panel/admin-app/src/components/CustomModal.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Modal } from 'antd';
+
+// coustom modal 
 const CustomModal = (props) => {
     const { open, hideModal , performAction , title } = props;
   return (
