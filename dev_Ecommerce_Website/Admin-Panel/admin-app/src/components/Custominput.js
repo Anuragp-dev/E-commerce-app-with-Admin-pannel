3c90172b825552d53d42286ffb07f910e601bd3b
ANURAG P <anuragp212@gmail.com>
2024-04-24 12:11:53 +0530

    Update Custominput.js

--- dev_Ecommerce_Website/Admin-Panel/admin-app/src/components/Custominput.js
+++ dev_Ecommerce_Website/Admin-Panel/admin-app/src/components/Custominput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// custom inputs
+// custom inputss
 const Custominput = (props) => {
     const {type  , label, i_id, i_className,name , value,onChange,onBlur} = props
   return (
