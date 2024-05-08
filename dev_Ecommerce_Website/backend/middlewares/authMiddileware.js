cfad0f81157282b0d5530651e3d7180ada010bf5
ANURAG P <anuragp212@gmail.com>
2024-05-08 13:01:40 +0530

    Update authMiddileware.js

--- dev_Ecommerce_Website/backend/middlewares/authMiddileware.js
+++ dev_Ecommerce_Website/backend/middlewares/authMiddileware.js
@@ -2,7 +2,7 @@ import  jwt  from "jsonwebtoken";
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
 
-
+// auth middileware
 
 export const authMiddleware = asyncHandler(async(req, res, next) => {
     let token;
