71e729c97b48f60c5289d2c68e12ca8fbbaab071
ANURAG P <anuragp212@gmail.com>
2024-07-06 11:28:31 +0530

    Update OpenRoutes.js

--- dev_Ecommerce_Website/Admin-Panel/admin-app/src/routing/OpenRoutes.js
+++ dev_Ecommerce_Website/Admin-Panel/admin-app/src/routing/OpenRoutes.js
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 
 
-// open routes for user
+// open routes for users
 
 export const OpenRoutes = ({children}) => {
     const getTokenFromLocalStorage = JSON.parse(localStorage.getItem("user"))
